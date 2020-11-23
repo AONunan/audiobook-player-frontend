c7ca47657316c5ad712578c355008e7fe4ecaf4a
Alan Noonan <aganoonan@gmail.com>
2020-11-23 17:37:27 +0000

    Remember timestamp of tracks

---
+++ README.md
@@ -0,0 +1,8 @@
+## Using virtualenv
+
+```bash
+pip3 install virtualenv
+python3 -m venv env
+source env/bin/activate # .\env\Scripts\Activate.ps1 # PowerShell
+deactivate
+```
--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,19 @@
+function setInitialValues(author, book, track, initialTimestamp) {
+  // Expand library sections
+  document.getElementById(author + "_list").style.display = "block";
+  document.getElementById(book + "_list").style.display = "block";
+
+  // Highlight current track in library
+  document.getElementById(author).style.color = "red";
+  document.getElementById(author + "/" + book).style.color = "red";
+  document.getElementById(author + "/" + book + "/" + track).style.color = "red";
+
+  // Set timestamp in media player
+  document.getElementById("player").currentTime = initialTimestamp;
+}
+
 function playTrack(author, book, track, test) {
-  // console.log("Track clicked.");
-  // console.log("Author:", author);
-  // console.log("Book:", book);
-  // console.log("Track:", track);
-
-  // console.log(test);
-
-  let trackUri = "http://192.168.1.2/audiobooks/" + author + "/" + book + "/" + track
-  let trackUriEncoded = encodeURI(trackUri);
-
+  // POST to API to set current track
   fetch("http://192.168.1.2:5200/set_current_track", {
     mode: 'no-cors',
     method: 'POST',
@@ -18,14 +23,29 @@ function playTrack(author, book, track, test) {
     body: JSON.stringify({
       'author': author,
       'book': book,
-      'track': track
+      'track': track,
+      'timestamp': 0
     }),
   })
 
-  let trackItems = document.getElementsByClassName("trackItem");
-  let i = 0;
-  for (i = 0; i < trackItems.length; i++) { trackItems[i].style.color = "white"; }
+  // Revert all highlighting in library
 
+  let i = 0;
+  let labels = [];
+
+  labels = document.getElementsByClassName("authorLabel");
+  for (i = 0; i < labels.length; i++) { labels[i].style.color = "white"; }
+
+  labels = document.getElementsByClassName("bookLabel");
+  for (i = 0; i < labels.length; i++) { labels[i].style.color = "white"; }
+
+  labels = document.getElementsByClassName("trackLabel");
+  for (i = 0; i < labels.length; i++) { labels[i].style.color = "white"; }
+
+  // Highlight currently playing track in library
+
+  document.getElementById(author).style.color = "red";
+  document.getElementById(author + "/" + book).style.color = "red";
   document.getElementById(author + "/" + book + "/" + track).style.color = "red";
 
   // Update current playing display
@@ -34,6 +54,9 @@ function playTrack(author, book, track, test) {
   document.getElementById("currentlyPlayingTrack").innerText = track;
 
   // Update media player track
+  let trackUri = "http://192.168.1.2/audiobooks/" + author + "/" + book + "/" + track
+  let trackUriEncoded = encodeURI(trackUri);
+
   let player = document.getElementById("player");
   player.src = trackUriEncoded;
   player.play();
@@ -52,35 +75,61 @@ function showHideSection(id) {
 
 function mediaControls(action) {
   let mediaPlayer = document.getElementById("player");
-  
+
   switch (action) {
     case "rewind":
       mediaPlayer.currentTime -= 5;
       break;
-  
+
     case "togglePlayPause":
       if (mediaPlayer.paused) {
         mediaPlayer.play();
       } else {
         mediaPlayer.pause();
-      }  
+      }
       break;
 
     case "fastForward":
       mediaPlayer.currentTime += 5;
       break;
-  
+
     default:
       console.log("No matching actions found.");
   }
 }
 
-function expandCurrentlyPlayingSectionOfLibrary(author, book, track) {
-  console.log("Author", author);
-  console.log("Book", book);
-  console.log("Track", track);
+let globalTimestamp = 0;
 
-  document.getElementById(author + "_list").style.display = "block";
-  document.getElementById(book + "_list").style.display = "block";
-  document.getElementById(author + "/" + book + "/" + track).style.color = "red";
-}
+window.setInterval(function () {
+
+  let mediaPlayer = document.getElementById("player");
+  let mediaPlayerTimestamp = Math.floor(mediaPlayer.currentTime);
+
+  if (globalTimestamp === mediaPlayerTimestamp) {
+    console.log("Timestamps the same, skipping");
+  } else {
+
+    globalTimestamp = Math.floor(mediaPlayer.currentTime);
+
+    let author = document.getElementById("currentlyPlayingAuthor").innerHTML;
+    let book = document.getElementById("currentlyPlayingBook").innerHTML;
+    let track = document.getElementById("currentlyPlayingTrack").innerHTML;
+
+    fetch("http://192.168.1.2:5200/set_current_track", {
+      mode: 'no-cors',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        'author': author,
+        'book': book,
+        'track': track,
+        'timestamp': globalTimestamp
+      }),
+    })
+  }
+
+}, 10000); // Loop every 10 seconds
+
+
